d28d4c2523d2948d12815908b92d7dc1215f61e6
manuelatrotta <manuelat88@hotmail.it>
2020-01-30 16:48:35 +0100

    inizio implemento scheletro ajax e if

--- js/script.js
+++ js/script.js
@@ -3,3 +3,17 @@
 //se è > di 5 il quadrato diventa verde.
 //Il numero ottenuto appare al centro del quadrato.
 //I quadrati fateli prima a mano e poi con javascript
+
+$.ajax({
+  url:"",
+  method:,
+  success:,
+  error:,
+
+});
+
+//if( numberRandom <= 5) {
+  $('.cella').addClass('yellow');
+}else{
+  $('cella').addClass('green');
+}
